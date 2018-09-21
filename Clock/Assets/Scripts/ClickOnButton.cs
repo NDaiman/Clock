83a61f8de1eadb3cff96659d9f780d04e970f1f8
NDaiman <42231386+NDaiman@users.noreply.github.com>
2018-09-21 14:17:49 +0300

    Make one class
    Make on class for buttons for more understandable and convenient work with ыскшзеы

---
+++ Clock/Assets/Scripts/ClickOnButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnButton : MonoBehaviour
+{
+    public AudioClip hitSound;
+    public Database database;
+    public GameObject arrow;
+    public int index;
+
+    void Awake()
+    {
+        if (!database)
+        {
+            database = Resources.Load("Database") as Database;
+        }
+
+        if (!arrow)
+        {
+            arrow = GameObject.Find("SecondArrow");
+        }
+    }
+
+    void Start()
+    {
+        index = 1;
+    }
+
+    void OnMouseDown()
+    {
+        gameObject.GetComponent<AudioSource>().clip = hitSound;
+        gameObject.GetComponent<AudioSource>().Play();
+
+        arrow.GetComponent<SpriteRenderer>().sprite = database.items[index].sprite;
+
+        if (index < database.items.Length - 1)
+        {
+            index++;
+        }
+        else
+        {
+            index--;
+        }
+    }
+}
